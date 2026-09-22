0ae87f19e2cada0c9c9f4439b61bf141307556b8
Myungha Lee <myhlee02@gmail.com>
2026-09-22 12:35:17 +0900

    init

--- nvim/lua/custom/plugins/toggleterm.lua
+++ nvim/lua/custom/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ vim.pack.add { 'https://github.com/akinsho/toggleterm.nvim' }
 
 require('toggleterm').setup {
   direction = 'horizontal',
-  shell = 'fish',
+  shell = 'bash',
 }
 
 vim.keymap.set('n', '<leader>t', '<cmd>ToggleTerm<CR>', { noremap = true, silent = true, desc = "Toggle Terminal" })
